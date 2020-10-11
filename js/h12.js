db4b1412973f6d697e1792650dc4844dfe787348
lv0719 <2596690984@qq.com>
2020-10-11 17:19:41 +0800

    第一次提交

---
+++ js/h12.js
@@ -0,0 +1,22 @@
+/*
+    请求地址：http://wthrcdn.etouch.cn/weather_mini
+    请求方法：get
+    请求参数：city(城市名)
+    响应内容：天气信息
+    // 功能：
+     1，点击回车
+     2，查询数据
+     3，渲染数据
+
+*/
+var app = new Vue({
+            el: "#app",
+            data: {
+
+            },
+            methods: {
+                searchWeather: function(){
+                    console.log(11);
+                }
+            }
+        })
